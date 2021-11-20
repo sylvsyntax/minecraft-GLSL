6d05e5dd48f98f250a8203ecd53a0846346b5138
TheAvonian <29378966+TheAvonian@users.noreply.github.com>
2021-11-20 05:12:56 -0500

    Adding stb requirement for textures

--- Project Files/main.cpp
+++ Project Files/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
 
 #include "shaderClass.h"
 #include "VAO.h"
@@ -12,21 +13,18 @@ const int WIDTH = 1280, HEIGHT = 720;
 const string PROJECT_FILES_ROOT = "../Project Files/";
 
 GLfloat vertices[] =
-{//					COORDINATES					 /		 COLORS			//
-	-0.5f, -0.5f * float(sqrt(3)) / 3,     0.0f,	0.8f, 0.3f,  0.02f, // Lower left corner
-	 0.5f, -0.5f * float(sqrt(3)) / 3,     0.0f,	0.8f, 0.3f,  0.02f, // Lower right corner
-	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,	1.0f, 0.6f,  0.32f, // Upper corner
-	-0.25f, 0.5f * float(sqrt(3)) / 6,	   0.0f,	0.9f, 0.45f, 0.17f, // Inner left
-	 0.25f, 0.5f * float(sqrt(3)) / 6,	   0.0f,	0.9f, 0.45f, 0.17f, // Inner right
-	 0.0f, -0.5f * float(sqrt(3)) / 3,     0.0f,	0.8f, 0.3f,  0.02f  // Inner down
+{//		 COORDINATES	 /		 COLORS		  //
+	-0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f, // Lower left corner
+	-0.5f,  0.5f, 0.0f,		0.0f, 1.0f, 0.0f, // Upper left corner
+	 0.5f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f, // Upper right corner
+	 0.5f, -0.5f, 0.0f,		1.0f, 1.0f, 1.0f  // Lower left corner
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 3, 5, // Lower left triangle
-	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle
+	0, 2, 1, // Upper triangle
+	0, 3, 2, // Lower triangle
 };
 
 
@@ -35,7 +33,7 @@ int main()
 	/**
 	*
 	*   Initialize GLFW
-	* {
+	* 
 	*****************************************/
 
 	glfwInit();
@@ -62,7 +60,7 @@ int main()
 
 
 	/*****************************************
-	* }
+	* 
 	*	End Initializing GLFW
 	* 
 	**/
@@ -71,7 +69,7 @@ int main()
 	/**
 	*
 	*   Adding Vertex and Shader Linking
-	* {
+	* 
 	*****************************************/
 
 	// Generates Shader object using shaders default.vert and default.frag
@@ -96,7 +94,7 @@ int main()
 	EBO1.Unbind();
 
 	/*****************************************
-	* }
+	* 
 	*	End Linking
 	*
 	**/
@@ -118,7 +116,7 @@ int main()
 		VAO1.Bind();
 
 		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
