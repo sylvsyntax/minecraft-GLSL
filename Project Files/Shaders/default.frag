1cd88d4011369c2ee15b1fee7b9203f283757e6a
TheAvonian <29378966+TheAvonian@users.noreply.github.com>
2021-11-20 06:30:58 -0500

    Adding texture data

--- Project Files/Shaders/default.frag
+++ Project Files/Shaders/default.frag
@@ -3,7 +3,12 @@ out vec4 FragColor;
 
 in vec3 color;
 
+in vec2 texCoord;
+
+
+uniform sampler2D tex0;
+
 void main()
 {
-	FragColor = vec4(color, 1.0);
+	FragColor = vec4(texture(tex0, texCoord).rgb, 1);
 }
--- Project Files/main.cpp
+++ Project Files/main.cpp
@@ -14,10 +14,10 @@ const string PROJECT_FILES_ROOT = "../Project Files/";
 
 GLfloat vertices[] =
 {//		 COORDINATES	 /		 COLORS		  //
-	-0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f, // Lower left corner
-	-0.5f,  0.5f, 0.0f,		0.0f, 1.0f, 0.0f, // Upper left corner
-	 0.5f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f, // Upper right corner
-	 0.5f, -0.5f, 0.0f,		1.0f, 1.0f, 1.0f  // Lower left corner
+	-0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+	-0.5f,  0.5f, 0.0f,		0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+	 0.5f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+	 0.5f, -0.5f, 0.0f,		1.0f, 1.0f, 1.0f,	1.0f, 0.0f, // Lower left corner
 };
 
 // Indices for vertices order
@@ -85,8 +85,9 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
@@ -101,6 +102,34 @@ int main()
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
+	int widthImg, heightImg, numColCh;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load("../Project Files/Textures/terrain.png", &widthImg, &heightImg, &numColCh, 0);
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+
+	glActiveTexture(GL_TEXTURE0);
+
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+
+	shaderProgram.Activate();
+	glUniform1i(tex0Uni, 0);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -111,7 +140,7 @@ int main()
 		// Tell Openwhich Shader Program we want to useGL 
 		shaderProgram.Activate();
 		glUniform1f(uniID, 0.5f);
-
+		glBindTexture(GL_TEXTURE_2D, texture);
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 
@@ -129,6 +158,7 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	glDeleteTextures(1, &texture);
 	shaderProgram.Delete();
 
 	// Delete window before ending the program
