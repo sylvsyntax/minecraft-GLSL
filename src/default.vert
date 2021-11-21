ebc5a17e43f661a5a06fb8ccc5d48fb577dff6f2
TheAvonian <29378966+TheAvonian@users.noreply.github.com>
2021-11-21 06:46:57 +0000

    Adding changes from visual-studio

--- src/default.vert
+++ src/default.vert
@@ -7,12 +7,14 @@ out vec3 color;
 
 out vec2 texCoord;
 
-uniform float scale;
+
+uniform mat4 camMatrix;
+
 
 void main()
 {
 	// projectionMatrix * viewMatrix * modelMatrix * vertexPosition
-	gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+	gl_Position = camMatrix * vec4(aPos, 1.0);
     color = aColor;
 	texCoord = aTex;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+// Authors: 
+// Sylvia and Aven
+// 
+// GitHub: 
+// https://github.com/sylvsyntax/minecraft-GLSL
+
+
+// SOURCES
+// 
+// 
+// Setup of GLFW, OpenGL, and most of the dependencies:
+// https://www.youtube.com/watch?v=45MIykWJ-C4
+// Setting up GLM for matrices in c++:
+// https://learnopengl.com/In-Practice/2D-Game/Rendering-Sprites
+
+
+
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -11,25 +28,58 @@
 #include "VAO.h"
 #include "VBO.h"
 #include "EBO.h"
+#include "Camera.h"
 
 
 const GLint WIDTH = 1280, HEIGHT = 720;
 
 GLfloat vertices[] =
 {//		 COORDINATES	 /		 COLORS		  /  TexCoord  //
-	-0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
-	-0.5f,  0.5f, 0.0f,		0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
-	 0.5f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
-	 0.5f, -0.5f, 0.0f,		1.0f, 1.0f, 1.0f,	1.0f, 0.0f, // Lower left corner
+	-0.5f, 0.0f,  0.5f,		0.83f, 0.70f, 0.44f,	0.0f, 0.0f, 
+	-0.5f, 0.0f, -0.5f,		0.83f, 0.70f, 0.44f,	5.0f, 0.0f, 
+	 0.5f, 0.0f, -0.5f,		0.83f, 0.70f, 0.44f,	0.0f, 0.0f, 
+	 0.5f, 0.0f,  0.5f,		0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,		0.92f, 0.86f, 0.76f,	2.5f, 5.0f,
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 2, 1, // Upper triangle
-	0, 3, 2, // Lower triangle
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4, 
+	3, 0, 4,
 };
 
+GLfloat lightVertices[] =
+{ //     COORDINATES       //
+	-0.1f, -0.1f,  0.1f,
+	-0.1f, -0.1f, -0.1f,
+	 0.1f, -0.1f, -0.1f,
+	 0.1f, -0.1f,  0.1f,
+	-0.1f,  0.1f,  0.1f,
+	-0.1f,  0.1f, -0.1f,
+	 0.1f,  0.1f, -0.1f,
+	 0.1f,  0.1f,  0.1f,
+};
+
+GLuint lightIndices[] =
+{
+	0, 1, 2,
+	0, 2, 3,
+	0, 4, 7,
+	0, 7, 3,
+	3, 7, 6,
+	3, 6, 2,
+	2, 6, 5,
+	2, 5, 1,
+	1, 5, 4,
+	1, 4, 0,
+	4, 5, 6,
+	4, 6, 7,
+};
 
 int main()
 {
@@ -104,7 +154,46 @@ int main()
 	*
 	**/
 
-	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+	/**
+	* 
+	*	Lighting
+	* 
+	******************************************/
+	
+	Shader lightShader("src/light.vert", "src/light.frag");
+
+	VAO lightVAO;
+	lightVAO.Bind();
+
+	VBO lightVBO(lightVertices, sizeof(lightVertices));
+	EBO lightEBO(lightIndices, sizeof(lightIndices));
+
+	lightVAO.LinkAttrib(lightVBO, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
+
+	lightVAO.Unbind();
+	lightVBO.Unbind();
+	lightEBO.Unbind();
+
+
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
+
+	glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::mat4 pyramidModel = glm::mat4(1.0f);
+	pyramidModel = glm::translate(pyramidModel, pyramidPos);
+
+	lightShader.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+	shaderProgram.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
+
+
+	/*****************************************
+	*
+	*	End Lighting
+	*
+	**/
 
 	// Texture
 
@@ -112,27 +201,43 @@ int main()
 
 	terrain.texUnit(shaderProgram, "tex0", 0);
 
-	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	// for textures not to be drawn wrong 
+	glEnable(GL_DEPTH_TEST);
 
-	shaderProgram.Activate();
-	glUniform1i(tex0Uni, 0);
+	Camera camera(WIDTH, HEIGHT, glm::vec3(0.0f, 0.0f, 2.0f));
+
+
+
+
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 
 		// Clean the back buffer and assign the new color to it
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+		camera.Inputs(window);
+		camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
 		// Tell Openwhich Shader Program we want to useGL 
 		shaderProgram.Activate();
-		glUniform1f(uniID, 0.5f);
+		camera.Matrix(shaderProgram, "camMatrix");
+
+
+		// binds texture so we can see it
 		terrain.Bind();
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 
 		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
+
+		lightShader.Activate();
+		camera.Matrix(lightShader, "camMatrix");
+		lightVAO.Bind();
+		glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
